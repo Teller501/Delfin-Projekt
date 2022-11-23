698d19b6677f9fae1cd9937229260a7eb0463495
Teller501 <ande777j@stud.kea.dk>
2022-11-23 08:44:50 +0100

    Register member method in Database

--- src/main/java/Database.java
+++ src/main/java/Database.java
@@ -3,5 +3,10 @@ import java.util.ArrayList;
 public class Database {
     private ArrayList<Member> members = new ArrayList<>();
 
+    public Member registerMember(String firstName, String lastName, int age, boolean isActive){
+        Member member = new Member(firstName, lastName, age, isActive);
+        members.add(member);
 
+        return member;
+    }
 }
